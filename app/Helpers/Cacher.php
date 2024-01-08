84b2cd97b8f43aae49dbddeb6b3e59d577952b5c
Mai Bui Xuan Hoan <mbxhoan001@gmail.com>
2024-01-08 11:39:16 +0700

    update Helper/Cacher

--- app/Helpers/Cacher.php
+++ app/Helpers/Cacher.php
@@ -12,14 +12,33 @@ class Cacher
         return self::return($cache, $returnType);
     }
 
-    public static function setRedis($prefix, $id)
+    public static function setRedis($prefix, $id, $data, $expirationMinutes = null)
     {
+        $key = "{$prefix}_{$id}";
+        $data = json_encode($data);
 
+        if ($expirationMinutes) {
+            Redis::setex($key, $expirationMinutes*60, $data);
+        } else {
+            Redis::set($key, $data);
+        }
+
+        return true;
     }
 
-    public static function updateRedis($prefix, $id)
+    public static function updateRedis($prefix, $id, $data, $expirationMinutes = null)
     {
+        $data = json_encode($data);
+        self::deleteRedis($prefix, $id);
+        self::setRedis($prefix, $id, $data, $expirationMinutes);
+        return true;
+    }
 
+    public static function deleteRedis($prefix, $id)
+    {
+        $key = "{$prefix}_{$id}";
+        Redis::del($key);
+        return true;
     }
 
     private function return($result, $type)
@@ -30,6 +49,7 @@ class Cacher
             case "array":
                 return json_decode($result, FALSE);
             default:
+                /* raw */
                 return $result;
         }
 
